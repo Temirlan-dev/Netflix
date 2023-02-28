2bec4a9a64bc9a41ffbca1aec7888030d5538f0b
Temirlan <aboutev.kg@gmail.com>
2023-02-28 12:52:36 +0600

    Скрываем при скролле Navigation Controller, так же добавили картинки

--- Netflix/Netflix/Controllers/HomeVC/HomeViewController.swift
+++ Netflix/Netflix/Controllers/HomeVC/HomeViewController.swift
@@ -29,5 +29,18 @@ class HomeViewController: UIViewController {
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         tableView.tableHeaderView = headerView
         setupTableView()
+        configureNavbar()
+    }
+    
+    private func configureNavbar() {
+        var image = UIImage(named: "netflixLogo")
+        image = image?.withRenderingMode(.alwaysOriginal)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
+        
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
+        ]
+        navigationController?.navigationBar.tintColor = .white
     }
 }
